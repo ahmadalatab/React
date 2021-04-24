1428eedb2cddc1a36fb966bed59d71635ba536c4
Ahmad Alatab <ahmadatab86@gmail.com>
2021-04-24 13:28:04 -0700

    added comment form in CampsiteInfo with Modal

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem  } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, FormGroup, Label } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
 
@@ -25,6 +26,7 @@ function RenderCampsite({campsite}){
                             <p>{comment.text}</p>
                             <p>--{comment.author}, -{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                         </div>)}
+                        <CommentForm />
                 </div>
             );
         }else{
@@ -56,6 +58,83 @@ function RenderCampsite({campsite}){
         else {
             return(
                 <div></div>
+            );
+        }
+    }
+
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+    class CommentForm extends Component {
+        constructor(props) {
+            super(props);
+            this.state={
+                isModalOpen: false
+            };
+            this.toggleModal = this.toggleModal.bind(this);
+        }
+
+        handleSubmit(values) {
+            console.log('Current state is: ' + JSON.stringify(values));
+            alert('Current state is: ' + JSON.stringify(values));
+        }
+
+        toggleModal() {
+            this.setState({
+                isModalOpen: !this.state.isModalOpen
+            });
+        }
+        render(){
+            return(
+                <>
+                <Button className="fa fa-pencil fa-lg" outline onClick={this.toggleModal}>Submit Comment</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <LocalForm className="ml-2 mr-2 mt-2" onSubmit={values => this.handleSubmit(values)}>
+                        <div className="form-group">
+                            <FormGroup>
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select name="rating" model=".rating" className="form-control">
+                                    <option value="none" selected disabled hidden>Choose a Rating</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </FormGroup>
+                        </div>
+                        <div className="form-group">
+                            <FormGroup>
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text name="author" model=".author" className="form-control" placeholder="Your Name" 
+                                        validators={{
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}                                    
+                                />
+                                <Errors
+                                    className="text-danger"
+                                    model=".author"
+                                    show="touched"
+                                    component="div"
+                                    messages={{
+                                        minLength: 'Must be at least 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                            </FormGroup>        
+                        </div>
+                        <div className="form-group">
+                            <FormGroup>
+                                <Label htmlFor="text">Comment</Label>
+                                <Control.textarea name="text" model=".text" className="form-control" rows="6"></Control.textarea>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Submit</Button>
+                        </div>
+                    </LocalForm>
+                </Modal>
+                </>
             );
         }
     }
--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -30,13 +30,13 @@ class Main extends Component {
                 partner={this.props.partners.filter(partner => partner.featured)[0]}
             />
           );
-      }
+      };
 
       const CampsiteWithId = ({match}) => {
           return(
             <CampsiteInfo 
-            campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-            comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
             />
           );
       }
@@ -45,10 +45,10 @@ class Main extends Component {
               <Header />
               <Switch>
                   <Route path='/home' component={HomePage} />
-                  <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                  <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
                   <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                   <Route exact path='/contactus' component={Contact} />
-                  <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
+                  <Route exact path='/aboutus' render={() => <About partners={this.props.partners} />} />
                   <Redirect to='/home'/>
               </Switch>
               <Footer />
